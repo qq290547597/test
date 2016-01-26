42f12d23ef89786ec5d3459024c0bd03df71ee78
李心洁 <lixinjie@4399inc.com>
2016-01-26 19:17:12 +0800

    test

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Lixinjie on 16/1/26.
+//  Copyright © 2016年 Lixinjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
